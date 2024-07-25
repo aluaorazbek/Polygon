99a330e329d16bbdf8f89dfdd8b92e4bcd8d89dd
naomiaori <alua@strait.finance>
2024-07-25 16:40:46 +0900

    Initial commit

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,5 @@
+const HomePage = () => (
+  <main className="relative mx-auto pb-[22.5rem] tablet:pb-[12rem] mobile:pb-[10rem] px-6 mobile:px-4 box-content" />
+);
+
+export default HomePage;
---
+++ src/views/header/index.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+import useLocale from "#/hooks/useLocale";
+
+const Header = () => {
+  const { t } = useTranslation("header");
+  const { locales, handleLocale } = useLocale();
+
+  return (
+    <header className="">
+      <div className="flex items-center gap-7">
+        {t("header_title")}
+        {t("nav_menu")}
+        {locales.map((locale) => (
+          <button
+            key={locale}
+            type="button"
+            className="bg-red-100"
+            onClick={() => {
+              handleLocale(locale);
+            }}
+          >
+            {locale}
+          </button>
+        ))}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
