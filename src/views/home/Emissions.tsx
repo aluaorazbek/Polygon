64260b06fba964f9e144cbcd4406faa476476325
naomiaori <alua@strait.finance>
2024-07-26 19:54:20 +0900

    fix: locales key naming

--- src/views/home/Emissions.tsx
+++ src/views/home/Emissions.tsx
@@ -7,7 +7,7 @@ import EmmisionsImg from "#/public/ui/emission.webp"
 const Emissions = () => {
 	const { t } = useTranslation('home')
 
-    const sections = t('scaling-section.public-chains', { returnObjects: true });
+    const sections = t('scaling_section.public_chains', { returnObjects: true });
     console.log(sections)
     const videoList = []
 	return (
@@ -15,13 +15,13 @@ const Emissions = () => {
             <div className="flex flex-col items-center justify-center px-40 py-95">
                 <div className="text-center text-white max-w-[600px] mb-40">
                     <h2 className="font-medium text-4xl mb-32 leading-tight tablet:text-xl">
-                        {t('emissions-section.title-1')} <br/>
-                        {t('emissions-section.title-2')}
+                        {t('emissions_section.title_1')} <br/>
+                        {t('emissions_section.title_2')}
                     </h2>
                     <p className="text-gray-100 text-2xl">
-                        {t('emissions-section.desc.part-1')}
-                        <span className="text-white">{t('emissions-section.desc.span')}</span>
-                        {t('emissions-section.desc.part-2')}
+                        {t('emissions_section.desc.part_1')}
+                        <span className="text-white">{t('emissions_section.desc.span')}</span>
+                        {t('emissions_section.desc.part_2')}
                     </p>
                 </div>
                 <button className='text-white'>Button</button>
